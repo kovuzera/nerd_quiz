5ec57e92d6d5b585fea9817fa24f0ce16e625293
Giovanne Marrone <giovannemarrone@gmail.com>
2021-09-16 15:50:01 -0300

    app created

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from "./Components/Header";
+import { GlobalStyle } from "./styles/global";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
